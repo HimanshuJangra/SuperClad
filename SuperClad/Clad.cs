f98ab6045dc529086b92490df6de7b4e337cfd4d
Himanshu Jangra <himanshujangra29@gmail.com>
2023-11-04 19:19:16 +0530

    initial commit

---
+++ SuperClad/Clad.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperClad
+{
+    public static class Clad
+    {
+        public static string ToUCase(this string input) => input.ToUpper() ?? string.Empty;
+        public static string ToLCase(this string input) => input.ToLower() ?? string.Empty;
+
+    }
+}
